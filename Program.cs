62f4a24e88286db3bb9874b99c1cd9e8c39e89a0
Mikael N <thedepq@gmail.com>
2020-08-17 09:30:04 +0300

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciseApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
