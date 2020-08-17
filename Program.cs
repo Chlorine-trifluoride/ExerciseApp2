7516976a7a1feb04039d4b7481cd2d4a299a3da1
Mikael N <thedepq@gmail.com>
2020-08-17 11:21:25 +0300

    Nice generic call method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using ExerciseApp2.Exercises;
 
 namespace ExerciseApp2
 {
     class Program
     {
+        private static bool exit = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (!exit)
+                PrintMainMenu();
+        }
+
+        static void RunExercise<T>() where T : IExercise, new()
+        {
+            Console.Clear();
+            new T().Run();
+            Console.ReadKey();
+        }
+
+        static void PrintMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Select Exercise to run:");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("A) Exercise 2A1");
+            Console.WriteLine("B) Exercise 2A2");
+            Console.WriteLine("C) Exercise 2A3");
+            Console.WriteLine("---------------");
+            Console.WriteLine("Q/E) Exit Program");
+
+            ConsoleKey key = Console.ReadKey().Key;
+
+            switch (key)
+            {
+                case ConsoleKey.A:
+                    RunExercise<Exercise2A1>();
+                    break;
+
+                case ConsoleKey.B:
+                    RunExercise<Exercise2A2>();
+                    break;
+
+                case ConsoleKey.C:
+                    RunExercise<Exercise2A3>();
+                    break;
+
+                case ConsoleKey.Q:
+                case ConsoleKey.E:
+                    exit = true;
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
