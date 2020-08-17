787ccdc7e69ccddbd8cc2c2f832d1d7c7cce82bc
Mikael N <thedepq@gmail.com>
2020-08-17 11:22:09 +0300

    Generics with some hacks

---
+++ Exercises/Exercise2A1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseApp2.Exercises
+{
+    class Exercise2A1 : IExercise
+    {
+        /*
+         * Write two methods Sum and Difference, which both have two parameters of type int, named value1 and value2. Sum should print “The sum of *value1* and
+         * *value2* is *sum*”, and Difference should print “The difference of *value1* and *value2* is *difference*.
+        Call the methods from the main method multiple times with different values.
+        */
+        public void Run()
+        {
+            Sum(1, 2);
+            Sum(2.5f, 4.5f);
+
+            Difference(2, 2);
+            Difference(6.5f, 3.5f);
+        }
+
+        private void Sum<T>(T value1, T value2)
+        {
+            dynamic a = value1;
+            dynamic b = value2;
+
+            try
+            {
+                Console.WriteLine($"The sum of {a} and {b} is {a + b})");
+            }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void Difference<T>(T value1, T value2)
+        {
+            dynamic a = value1;
+            dynamic b = value2;
+
+            try
+            {
+                Console.WriteLine($"The difference of {a} and {b} is {a - b})");
+            }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
