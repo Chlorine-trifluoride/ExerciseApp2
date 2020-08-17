10a77c59408f04b11411be5ce6cdf078322f5da9
Mikael N <thedepq@gmail.com>
2020-08-17 11:22:29 +0300

    Exercise 2A2 and 2A3

---
+++ Exercises/Exercise2A2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ExerciseApp2.Exercises
+{
+    static class ExerciseStringExtensions
+    {
+        public static string RemoveSpaces(this string input)
+        {
+            // I don't like this approach
+            char[] charArray = input.Where(c => c != ' ').ToArray();
+            string output = new string(charArray);
+
+            return output;
+        }
+    }
+
+    class Exercise2A2 : IExercise
+    {
+        public void Run()
+        {
+            PrintNumSpaces(GLOBALS.TEST_STRING);
+
+            string testResult = GLOBALS.TEST_STRING.RemoveSpaces();
+            Console.WriteLine(testResult);
+        }
+
+        private void PrintNumSpaces(string input)
+        {
+            int numSpaces = input.Count(x => x == ' ');
+            Console.WriteLine($"Number of spaces: {numSpaces}");
+        }
+    }
+}
---
+++ Exercises/Exercise2A3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExerciseApp2.Exercises
+{
+    class Exercise2A3 : IExercise
+    {
+        public void Run()
+        {
+            Console.WriteLine($"List of distrinct characters in {GLOBALS.TEST_STRING}");
+            PrintAllDistinctCharacters(GLOBALS.TEST_STRING);
+            Console.WriteLine("--------");
+            Console.WriteLine($"Number of unique letters in {GLOBALS.TEST_STRING}");
+            PrintNumDistinctCharacters(GLOBALS.TEST_STRING);
+        }
+
+        // Write a method that takes a string as a parameter and prints the number of each unique letter in the string.
+        private void PrintNumDistinctCharacters(string input)
+        {
+            string stringWithOnlyCharacters = StringWithOnlyCharacters(input);
+
+            char[] characters = stringWithOnlyCharacters.Distinct().ToArray();
+
+            foreach (char c in characters)
+            {
+                int numChar = input.Count(x => x == c);
+                Console.WriteLine($"Num of {c}'s: {numChar}");
+            }
+        }
+
+        //Write a method that takes a string as a parameter and prints each unique letter in that string.
+        private void PrintAllDistinctCharacters(string input)
+        {
+            string stringWithOnlyCharacters = StringWithOnlyCharacters(input);
+            Console.WriteLine(GetUniqueCharacters(stringWithOnlyCharacters));
+        }
+
+        private string StringWithOnlyCharacters(string input)
+        {
+            // why is there no global regex in C#? Am I missing something
+            var matches = Regex.Matches(input, @"[a-z]", RegexOptions.IgnoreCase);
+
+            var matchArray = from Match match in matches
+                             select match.Value;
+
+            return String.Join("", matchArray);
+        }
+
+        private char[] GetUniqueCharacters(string input)
+        {
+            return input.Distinct().ToArray();
+        }
+    }
+}
